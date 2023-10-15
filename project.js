7b965ada9b71fa40af7246df3a3e1b96a09189a6
Kutay Akpinar <kutayakpnar@gmail.com>
2023-10-15 20:18:00 +0300

    Cars project from udemy course

---
+++ project.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("car-form");
+const titleElement = document.querySelector("#title");
+const priceElement = document.querySelector("#price");
+const urlElement = document.querySelector("#url");
+const cardbody = document.querySelectorAll(".card-body")[1];
+const clear = document.getElementById("clear-cars");
+
+//UI INITIALIZE
+
+const ui = new UI();
+const storage = new Storage();
+
+
+//Load all events
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addCar);
+    document.addEventListener("DOMContentLoaded", function () {
+        let cars = storage.getCarsFromStorage();
+        ui.loadAllCars(cars);
+
+    })
+    cardbody.addEventListener("click", deleteCar);
+    clear.addEventListener("click", clearAllCars);
+
+}
+
+function addCar(e) {
+    const title = titleElement.value;
+    const price = priceElement.value;
+    const url = urlElement.value;
+
+    if (title === "" || price === "" || url === "") {
+        //Error
+        ui.displayMessage("Fill form.", "danger");
+    }
+    else {
+        //New Car
+        const newCar = new Car(title, price, url);
+        ui.addCarToUI(newCar) //Add car to ui
+        storage.addCarToStorage(newCar);
+        ui.displayMessage("Car have added successfully.", "success");
+    }
+
+
+    ui.clearInputs(titleElement, urlElement, priceElement)
+    e.preventDefault();
+
+}
+
+function deleteCar(e) {
+    if (e.target.id === "delete-car") {
+        ui.deleteCarFromUI(e.target);
+        storage.deleteCarFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        console.log(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        e.preventDefault();
+
+    }
+    ui.displayMessage("Car has deleted,successfully.", "success");
+}
+
+function clearAllCars() {
+
+
+    if (confirm("You will delete all cars.Are you sure?")){
+        ui.clearAllCarsFromUI();
+        storage.clearAllCarsFromStorage();
+
+    };
+}
